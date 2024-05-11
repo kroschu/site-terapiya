93c58bf151ea51ad79b3f97af10c4f602c7ae14f
kroschu <tukroschu@gmail.com>
2024-05-11 21:54:01 +0300

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -43,22 +43,20 @@ module.exports = function (eleventyConfig) {
 `;
   });
 
-eleventyConfig.addShortcode("tiktokInfinite", (videoURL) => {
+eleventyConfig.addShortcode("tiktok", (videoURL) => {
     const url = new URL(videoURL);
     const videoId = url.pathname.split("/").pop();
     if (!videoId) {
         console.error("Unable to extract video ID from the provided URL:", videoURL);
         return ""; // Return empty string if video ID cannot be extracted
     }
-
-    let tiktokEmbed = `
+    return `
 <blockquote class="tiktok-embed" cite="${videoURL}" data-video-id="${videoId}">
   <section> </section>
 </blockquote>
 <script async src="https://www.tiktok.com/embed.js"></script>
 `;
 });
-
 
   eleventyConfig.setLiquidOptions({
     dynamicPartials: true,
